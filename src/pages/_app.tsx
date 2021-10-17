6c35afd333b59a3e43abe6fc3189894fce967237
Erika Perciliano <erikaperciliano.developer@gmail.com>
2021-10-17 14:26:40 -0300

    add context
    - it has all context information

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,12 @@
-import { AppProps } from 'next/app';
+import { AuthProvider } from '../context/AuthContext';
+import '../styles/global.scss';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps }) {
+  return (
+    <AuthProvider>
+      <Component {...pageProps} />
+    </AuthProvider>
+  )
 }
 
 export default MyApp
