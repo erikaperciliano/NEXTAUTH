785f698ef113fa98acf56a9834798c02b2958e5a
Erika Perciliano <erikaperciliano.developer@gmail.com>
2021-10-28 22:37:39 -0300

    working with cookies at server side render
    - If exist token  the user can log in

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,7 @@
+import { GetServerSideProps } from 'next';
 import { FormEvent, useContext, useState } from 'react';
+import { parseCookies } from 'nookies';
+
 import { AuthContext } from '../context/AuthContext';
 import styles from '../styles/Home.module.scss';
 
@@ -26,4 +29,21 @@ export default function Home() {
       <button type="submit">Entrar</button>
     </form>
   )
+}
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  const cookies = parseCookies(ctx);
+
+  if(cookies['nextauth.token']) {
+    return {
+      redirect: {
+        destination: '/dashboard',
+        permanent: false
+      }
+    }
+  }
+
+  return {
+    props: {}
+  }
 }
